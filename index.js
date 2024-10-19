080541db29bcb1543234f9bbad7d0d538ae9fa03
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-10-18 23:45:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 questions = {
-  "Why Did They Wear Masks For The Black Death?": "They Thought The Smell Made People Sick",
-  "Why Wernt Jews Affected By Black Death?": "They Were Clean And Refused To Eat Dirty Foods"
+	"Why Did They Wear Masks For The Black Death?": "They Thought The Smell Made People Sick",
+	"Why Wernt Jews Affected By Black Death?": "They Were Clean And Refused To Eat Dirty Foods",
+	"What Happened To The Jews During Black Death?" : "Prosecuted",
+	"What Were The Jews Accused Of During Black Death?": "Poisoning The Wells",
+	"How Was The Black Death Transmitted?": "Fleas On Rats"
 }
 
 var currentQuestion = 0
